ea1563f9ffc8ab9f08960313a899c56687adc7bf
Schizoid <123573724+Nirek-Gulia@users.noreply.github.com>
2024-11-11 11:18:04 +0530

    Update 87.py

--- 87.py
+++ 87.py
@@ -3,4 +3,4 @@
 import math_utils as mu 
 result = mu.add(5, 45) 
 print(result)
-print("87. This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("87. This program is written by Nirek Gulia 0221BCA046")
