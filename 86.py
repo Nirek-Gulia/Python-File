a2a4c862190f9ad887812d087471f25f67493014
Schizoid <123573724+Nirek-Gulia@users.noreply.github.com>
2024-11-11 11:18:22 +0530

    Update 86.py

--- 86.py
+++ 86.py
@@ -3,4 +3,4 @@
 from math_utils import * 
 result = subtract(10, 8) 
 print(result)
-print("86. This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("86. This program is written by Nirek Gulia 0221BCA046")
